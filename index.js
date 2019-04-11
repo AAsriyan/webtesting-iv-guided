fef0539539c3bb65622d94428de67db40a988a05
AAsriyan <aasriyan@protonmail.com>
2019-04-11 14:38:43 -0400

    Added notes for deploying on heroku with pg

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const server = require('./api/server.js');
+const server = require("./api/server.js");
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** server up on port ${port} **\n`));
+
+// Setting up PG on Heroku
+//  * Head to Heroku
+//  * Set up new application
+//  * Sync from GitHub
+//  * Resources > Add-ons > Heroku Postgres
+// 	 * Automatically adds config variable in settings
+//  * Change Knex configuration
+// 	 * Add production key using new DB
+// 		 * client: pg
+// 		 * connection: DATABASE_URL || localPG {host, database, user, password}
+// 		 * same migrations & seeds as before
+//  * Change dbEnv in dbConfig
+// 	 * DB_ENV || 'production'
+//  * Commit & push changes
+//  * Set DB_ENV config variable on Heroku
+//  * Run migrations on Heroku
+// 	 * `npx heroku run knex migrate:latest -a <application>`
